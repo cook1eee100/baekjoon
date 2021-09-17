6e44dcff8a53ce150d75d28bf919b6ce9bc87591
cook1eee100 <kahtyi@outlook.kr>
2021-09-17 21:47:40 +0900

    2021/09/17 greedy algorithm

--- algorithm/greedy/1439_turnOver.py
+++ algorithm/greedy/1439_turnOver.py
@@ -1,10 +1,9 @@
-while True:
-    s = input()
+s = input()
 
-    answer=0
-    for i in range(1, len(s)):
-        if s[0]!=s[i] and s[i-1]!=s[i]:
-            answer+=1
+answer=0
+for i in range(1, len(s)):
+    if s[0]!=s[i] and s[i-1]!=s[i]:
+        answer+=1
 
-    print(answer)
+print(answer)
     
